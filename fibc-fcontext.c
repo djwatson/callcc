098b1e3f7e16367a619772318349a611e7799cc1
Dave Watson <dade.watson@gmail.com>
2025-01-11 12:12:52 -0800

    Working hack for fibc

---
+++ fibc-fcontext.c
@@ -0,0 +1,118 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static int64_t succ(int64_t n) { return n + 1; }
+static int64_t pred(int64_t n) { return n - 1; }
+
+typedef struct clo {
+  int64_t (*ptr)(void* data, int64_t val);
+  void* clo_data;
+  int64_t res;
+} clo;
+
+
+
+static int64_t call_clo(clo* clo, int64_t x) {
+  return clo->ptr(clo, x);
+};
+
+typedef int64_t(*ccthunk)(clo*, int64_t);
+
+static int64_t thunk(void* unused, int64_t n) { return n; }
+
+typedef struct ccsave {
+  int64_t (*ptr)(void* data, int64_t val);
+  void* clo_data;
+  int64_t res;
+
+  void* stack;
+  size_t sz;
+} ccsave;
+
+void* stacktop;
+
+static int64_t ccresthunk(void* unused, int64_t n) {
+  ccsave* c = (ccsave*)unused;
+  c->res = n;
+
+  register int64_t stack_bottom asm("rdi") = (int64_t)stacktop - c->sz;
+  register void* saved_stack asm ("rsi") = c->stack;
+  register size_t saved_sz asm ("rcx") = c->sz;
+
+   asm volatile (
+          	 "mov %2, %%rsp\n\t" // Restore the old stack pointer.
+		 "rep movsb\n\t" // Copy the old stack over (copy rsi -> rdi, rcx times)
+		 "pop %%rbp\n\t" // Pop the old frame pointer.
+		 "mov $1, %%rax\n\t" // Set return value to 1, indicating this is the second return.
+		 "ret\n\t" // Return to caller of 'getstack'.
+		: // output
+		: "r" (saved_stack), "r" (saved_sz), "r" (stack_bottom)//input
+		: "rax", "rsp", "rbp", "memory"// clobbers
+		);
+   __builtin_unreachable();
+}
+
+// *MUST* have a frame pointer...
+__attribute__((returns_twice, noinline)) static int getstack(ccsave *res) [[clang::preserve_none]]  {
+  void* stack_bottom = __builtin_frame_address(0);
+  size_t stack_sz = stacktop - stack_bottom;
+  printf("Stack size %li\n", stack_sz);
+  res->stack = malloc(stack_sz);
+  memcpy(res->stack, stack_bottom, stack_sz);
+  res->sz = stack_sz;
+  return res->res;
+}
+
+static int64_t callcc(ccthunk t, int64_t x) {
+  ccsave* stack = malloc(sizeof(ccsave));
+  stack->ptr = ccresthunk;
+  stack->clo_data = stack;
+  stack->res = 0;
+  int res = getstack(stack);
+  if (res) { // This was the second return.
+    free(stack->stack);
+    free(stack);
+    return stack->res;
+  }
+  return t((clo*)stack, x);
+}
+
+static int64_t addc(int64_t x, int64_t y, clo* k) {
+  if (y == 0) {
+    return call_clo(k, x);
+  }
+  return addc(succ(x), pred(y), k);
+}
+
+static int64_t fibc(int64_t x, clo* c);
+static int64_t cc1(clo* c, int64_t x) {
+  return fibc(pred(x), c);
+}
+
+static int64_t cc2(clo *c, int64_t x) { return fibc(pred(pred(x)), c); }
+
+static int64_t fibc(int64_t x, clo* c) {
+  if (x == 0) {
+    return call_clo(c, 0);
+  }
+  if (pred(x) == 0) {
+    return call_clo(c, 1);
+  }
+  int64_t arg1 = callcc(cc1, x);
+  int64_t arg2 = callcc(cc2, x);
+  return addc(arg1, arg2, c);
+}
+
+int main() {
+  int foobar;
+  stacktop = &foobar;
+  // iter count 10
+  for(int64_t i = 0; i < 10; i++) {
+    clo c = {thunk, NULL};
+    int64_t res = fibc(30, &c);
+    printf("Res %li\n", res);
+  }
+  return 0;
+}
