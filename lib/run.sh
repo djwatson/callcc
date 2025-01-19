ebd33338e0fc353cbbfb8306e6d82c82d485dd8e
Dave Watson <dade.watson@gmail.com>
2025-01-19 10:04:46 -0800

    run

---
+++ lib/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+FILE=$(basename $1 .scm)
+gosh -I. bc.gauche.scm $1 > test.ll 2>/dev/null
+
+clang -DNDEBUG -flto -O3 -g  -std=gnu23 -o $FILE test.ll ../types.c ../gc.c ../list.c ../alloc_table.c
