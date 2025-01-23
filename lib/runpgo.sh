4d240c1db75edafabd4decf01312cbd1f6d1943a
Dave Watson <dade.watson@gmail.com>
2025-01-23 13:53:58 -0800

    PGO script

---
+++ lib/runpgo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+FILE=$(basename $1 .scm)
+gosh -I. bc.gauche.scm $1 > test.ll 2>/dev/null && clang   -O3 -DNDEBUG -flto  -g  -std=gnu23 -o $FILE test.ll ../types.c ../gc.c ../list.c ../alloc_table.c -lm -fprofile-generate || echo "FAIL $FILE"
+LLVM_PROFILE_FILE=a.profraw ./$FILE
+llvm-profdata merge -output=a.profdata a.profraw
+clang -DNDEBUG -flto -O3 -g  -o $FILE  -std=gnu23  test.ll ../types.c ../gc.c ../list.c ../alloc_table.c   -lm  -fprofile-use=a.profdata
+
+time ./$FILE
