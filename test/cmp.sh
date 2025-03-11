be6e9ccee1823c5427997e524a1a775d0e271ce2
Dave Watson <dade.watson@gmail.com>
2025-03-11 08:53:10 -0700

    r7rs tests

--- test/cmp.sh
+++ test/cmp.sh
@@ -1,3 +1,7 @@
 name=${1%.*}
-../callcc -fno-eval --exe $1 2>/dev/null > $name.ll
+if [ "$1" = "../test/r7rs-tests.scm" ]; then
+    ../callcc --exe $1 2>/dev/null > $name.ll
+else
+    ../callcc -fno-eval --exe $1 2>/dev/null > $name.ll
+fi
 diff -Naur <(./$name) $name.out
